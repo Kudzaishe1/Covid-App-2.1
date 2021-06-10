c75a751177dcbc35f09e3d00851b475cc509d1da
chadmul <51158724+chadmul@users.noreply.github.com>
2021-06-10 16:05:23 +0200

    CFirst Commit

--- Models/UserDb.cs
+++ Models/UserDb.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Covid_App_2._0.Models
 {
     public class UserDb
     {
+        [Key]
+        public int email { get; set; }
+        public string Name { get; set; }
+      
+        
+        public string Employee { get; set; }
+
+        public string type { get; set; }
     }
 }
